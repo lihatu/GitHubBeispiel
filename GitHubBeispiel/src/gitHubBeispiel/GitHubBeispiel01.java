9e26830a4c10ddd6abc02b516bf6eb04bb287932
Administrator <Administrator@DT-B-2103>
2024-02-27 16:42:46 +0100

    third commit

--- GitHubBeispiel/src/gitHubBeispiel/GitHubBeispiel01.java
+++ GitHubBeispiel/src/gitHubBeispiel/GitHubBeispiel01.java
@@ -4,6 +4,7 @@ public class GitHubBeispiel01 {
 	public static void main(String[] args) {
 		System.out.println("Hallo Jan" );
 		System.out.println("Hallo Rene" );
+		System.out.println("Hallo Linda");
 	}
 }
 	
