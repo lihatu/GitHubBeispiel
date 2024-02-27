498f006b4911b6651f86786065330745c2b36858
Administrator <Administrator@DT-B-2103>
2024-02-27 16:08:23 +0100

    second commit

--- GitHubBeispiel/src/gitHubBeispiel/GitHubBeispiel01.java
+++ GitHubBeispiel/src/gitHubBeispiel/GitHubBeispiel01.java
@@ -3,5 +3,7 @@ package gitHubBeispiel;
 public class GitHubBeispiel01 {
 	public static void main(String[] args) {
 		System.out.println("Hallo Jan" );
+		System.out.println("Hallo Rene" );
 	}
 }
+	
